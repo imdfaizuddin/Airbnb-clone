f42d8eeebb015fd73737f781bba230fc6ef17e7a
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-19 23:55:59 +0530

    connect to db

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const mongoose = require("mongoose");
 
 const PORT = 3000;
 
+const MONGO_URL ='mongodb://127.0.0.1:27017/airbnb';
+
+main().then(res=>{
+    console.log("connected to DB");
+}).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+  }
+
 app.get("/", (req,res)=>{
     res.send("working");
 });
