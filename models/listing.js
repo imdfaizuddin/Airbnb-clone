93e74eefa6d01a2a9dbe790907ebaaa7b93689ac
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-21 00:37:30 +0530

    initialize data

--- models/listing.js
+++ models/listing.js
@@ -9,9 +9,12 @@ const listingSchema = new Schema({
     },
     description: String,
     image:{
+        filename: String,
+        url:{
         type:String,
         default: "https://images.unsplash.com/photo-1581847797580-a3ac8010d37c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v)=> v === "" ? "https://images.unsplash.com/photo-1581847797580-a3ac8010d37c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        },
     } ,
     price: Number,
     location: String,
