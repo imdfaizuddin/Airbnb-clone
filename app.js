0e58928c1cee79d33491e8f7320ca856982a120c
Md Faizuddin <imdfaizuddin@gmail.com>
2024-02-21 23:16:06 +0530

    all listing route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const path = require("path");
 const Listing = require("./models/listing.js");
 const PORT = 3000;
 
@@ -13,25 +14,32 @@ main().then(res=>{
 
 async function main() {
     await mongoose.connect(MONGO_URL);
-  }
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
 
 app.get("/", (req,res)=>{
     res.send("working");
 });
 
-app.get("/testListing", async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-
-    await sampleListing.save();
-    console.log("sample listing was saved");
-    res.send("successful testing");
+app.get("/listings", async (req, res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
 });
+// app.get("/testListing", async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample listing was saved");
+//     res.send("successful testing");
+// });
 
 app.listen(PORT , ()=>{
     console.log("server is running on port: ", PORT);
